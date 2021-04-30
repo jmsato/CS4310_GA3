b9519f37e6da8a65363e20803282aa2d0c03fdf3
jmsato <jordyn.sato@gmail.com>
2021-04-30 12:11:33 -0700

    Added Request class

--- src/Request.java
+++ src/Request.java
@@ -1,4 +1,31 @@
 
 public class Request {
+	private int size;
+	private double memoryUtilization;
+	
+	public Request(int size, double memUtil) {
+		this.size = size;
+		this.memoryUtilization = memUtil;
+	}
 
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public double getMemoryUtilization() {
+		return memoryUtilization;
+	}
+
+	public void setMemoryUtilization(double memoryUtilization) {
+		this.memoryUtilization = memoryUtilization;
+	}
+	
+	public String toString() {
+		return "Request [ Size: " + this.size +
+				", Memory Utilization when requesting: " + this.memoryUtilization + " ]";
+	}
 }
