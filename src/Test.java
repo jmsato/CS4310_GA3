eca81a14513d9094f05f518cb8d925c9d8959e54
jmsato <jordyn.sato@gmail.com>
2021-05-04 17:03:31 -0700

    Changed csv rows

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,22 @@ import java.util.List;
 
 public class Test {
 
+	private static double getAverageMemUtil(Request[] req) {
+		double mem = 0;
+		for (Request r : req) {
+			mem += r.getMemoryUtilization();
+		}
+		return mem / req.length;
+	}
+	
+	private static double getAverageHoles(Request[] req) {
+		int holes = 0;
+		for (Request r : req) {
+			holes += r.getNumHoles();
+		}
+		return holes / req.length;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Running Test.java file...");
 		int runTests = 1000;
@@ -28,6 +44,8 @@ public class Test {
 			holeRows.add(new String[]{"Best fit test suite:"});
 			
 			//Test Case 1: d = 100, v = 0
+			memRows.add(new String[]{"d = 100 and v = 0"});
+			holeRows.add(new String[]{"d = 100 and v = 0"});
 			for(int times = 0; times < runTests; times++) {	
 				//Create a new row for each test run
 				String[] memR = new String[4];
@@ -35,23 +53,25 @@ public class Test {
 				//Run simulation
 				Simulation sim1 = new Simulation(100, 0);
 				Request[] first = sim1.runFirstFit();
-				memR[0] = Double.toString(first.getAverageMemUtil());
-				holeR[0] = Double.toString(first.getAverageHoles());
+				memR[0] = Double.toString(getAverageMemUtil(first));
+				holeR[0] = Double.toString(getAverageHoles(first));
 				Request[] next = sim1.runNextFit();
-				memR[1] = Double.toString(next.getAverageMemUtil());
-				holeR[1] = Double.toString(next.getAverageHoles());
+				memR[1] = Double.toString(getAverageMemUtil(next));
+				holeR[1] = Double.toString(getAverageHoles(next));
 				Request[] best = sim1.runBestFit();
-				memR[2] = Double.toString(best.getAverageMemUtil());
-				holeR[2] = Double.toString(best.getAverageHoles());
+				memR[2] = Double.toString(getAverageMemUtil(best));
+				holeR[2] = Double.toString(getAverageHoles(best));
 				Request[] worst = sim1.runWorstFit();
-				memR[3] = Double.toString(worst.getAverageMemUtil());
-				holeR[3] = Double.toString(worst.getAverageHoles());
+				memR[3] = Double.toString(getAverageMemUtil(worst));
+				holeR[3] = Double.toString(getAverageHoles(worst));
 				//Add rows to csv files
 				memRows.add(memR);
 				holeRows.add(holeR);
 			}
 			
 			//Test Case 2: d = 100, v = 10
+			memRows.add(new String[]{"d = 100 and v = 10"});
+			holeRows.add(new String[]{"d = 100 and v = 10"});
 			for(int times = 0; times < runTests; times++) {	
 				//Create a new row for each test run
 				String[] memR = new String[4];
@@ -59,23 +79,25 @@ public class Test {
 				//Run simulation
 				Simulation sim2 = new Simulation(100, 10);
 				Request[] first = sim2.runFirstFit();
-				memR[0] = Double.toString(first.getAverageMemUtil());
-				holeR[0] = Double.toString(first.getAverageHoles());
+				memR[0] = Double.toString(getAverageMemUtil(first));
+				holeR[0] = Double.toString(getAverageHoles(first));
 				Request[] next = sim2.runNextFit();
-				memR[1] = Double.toString(next.getAverageMemUtil());
-				holeR[1] = Double.toString(next.getAverageHoles());
+				memR[1] = Double.toString(getAverageMemUtil(next));
+				holeR[1] = Double.toString(getAverageHoles(next));
 				Request[] best = sim2.runBestFit();
-				memR[2] = Double.toString(best.getAverageMemUtil());
-				holeR[2] = Double.toString(best.getAverageHoles());
+				memR[2] = Double.toString(getAverageMemUtil(best));
+				holeR[2] = Double.toString(getAverageHoles(best));
 				Request[] worst = sim2.runWorstFit();
-				memR[3] = Double.toString(worst.getAverageMemUtil());
-				holeR[3] = Double.toString(worst.getAverageHoles());
+				memR[3] = Double.toString(getAverageMemUtil(worst));
+				holeR[3] = Double.toString(getAverageHoles(worst));
 				//Add rows to csv files
 				memRows.add(memR);
 				holeRows.add(holeR);
 			}
 			
 			//Test Case 3: d = 100, v = 50
+			memRows.add(new String[]{"d = 100 and v = 50"});
+			holeRows.add(new String[]{"d = 100 and v = 50"});
 			for(int times = 0; times < runTests; times++) {	
 				//Create a new row for each test run
 				String[] memR = new String[4];
@@ -83,23 +105,25 @@ public class Test {
 				//Run simulation
 				Simulation sim3 = new Simulation(100, 50);
 				Request[] first = sim3.runFirstFit();
-				memR[0] = Double.toString(first.getAverageMemUtil());
-				holeR[0] = Double.toString(first.getAverageHoles());
+				memR[0] = Double.toString(getAverageMemUtil(first));
+				holeR[0] = Double.toString(getAverageHoles(first));
 				Request[] next = sim3.runNextFit();
-				memR[1] = Double.toString(next.getAverageMemUtil());
-				holeR[1] = Double.toString(next.getAverageHoles());
+				memR[1] = Double.toString(getAverageMemUtil(next));
+				holeR[1] = Double.toString(getAverageHoles(next));
 				Request[] best = sim3.runBestFit();
-				memR[2] = Double.toString(best.getAverageMemUtil());
-				holeR[2] = Double.toString(best.getAverageHoles());
+				memR[2] = Double.toString(getAverageMemUtil(best));
+				holeR[2] = Double.toString(getAverageHoles(best));
 				Request[] worst = sim3.runWorstFit();
-				memR[3] = Double.toString(worst.getAverageMemUtil());
-				holeR[3] = Double.toString(worst.getAverageHoles());
+				memR[3] = Double.toString(getAverageMemUtil(worst));
+				holeR[3] = Double.toString(getAverageHoles(worst));
 				//Add rows to csv files
 				memRows.add(memR);
 				holeRows.add(holeR);
 			}
 			
 			//Test Case 4: d = 100, v = 90
+			memRows.add(new String[]{"d = 100 and v = 90"});
+			holeRows.add(new String[]{"d = 100 and v = 90"});
 			for(int times = 0; times < runTests; times++) {	
 				//Create a new row for each test run
 				String[] memR = new String[4];
@@ -107,23 +131,25 @@ public class Test {
 				//Run simulation
 				Simulation sim4 = new Simulation(100, 90);
 				Request[] first = sim4.runFirstFit();
-				memR[0] = Double.toString(first.getAverageMemUtil());
-				holeR[0] = Double.toString(first.getAverageHoles());
+				memR[0] = Double.toString(getAverageMemUtil(first));
+				holeR[0] = Double.toString(getAverageHoles(first));
 				Request[] next = sim4.runNextFit();
-				memR[1] = Double.toString(next.getAverageMemUtil());
-				holeR[1] = Double.toString(next.getAverageHoles());
+				memR[1] = Double.toString(getAverageMemUtil(next));
+				holeR[1] = Double.toString(getAverageHoles(next));
 				Request[] best = sim4.runBestFit();
-				memR[2] = Double.toString(best.getAverageMemUtil());
-				holeR[2] = Double.toString(best.getAverageHoles());
+				memR[2] = Double.toString(getAverageMemUtil(best));
+				holeR[2] = Double.toString(getAverageHoles(best));
 				Request[] worst = sim4.runWorstFit();
-				memR[3] = Double.toString(worst.getAverageMemUtil());
-				holeR[3] = Double.toString(worst.getAverageHoles());
+				memR[3] = Double.toString(getAverageMemUtil(worst));
+				holeR[3] = Double.toString(getAverageHoles(worst));
 				//Add rows to csv files
 				memRows.add(memR);
 				holeRows.add(holeR);
 			}
 			
 			//Test Case 5: d = 100, v = 99
+			memRows.add(new String[]{"d = 100 and v = 99"});
+			holeRows.add(new String[]{"d = 100 and v = 99"});
 			for(int times = 0; times < runTests; times++) {	
 				//Create a new row for each test run
 				String[] memR = new String[4];
@@ -131,17 +157,17 @@ public class Test {
 				//Run simulation
 				Simulation sim5 = new Simulation(100, 99);
 				Request[] first = sim5.runFirstFit();
-				memR[0] = Double.toString(first.getAverageMemUtil());
-				holeR[0] = Double.toString(first.getAverageHoles());
+				memR[0] = Double.toString(getAverageMemUtil(first));
+				holeR[0] = Double.toString(getAverageHoles(first));
 				Request[] next = sim5.runNextFit();
-				memR[1] = Double.toString(next.getAverageMemUtil());
-				holeR[1] = Double.toString(next.getAverageHoles());
+				memR[1] = Double.toString(getAverageMemUtil(next));
+				holeR[1] = Double.toString(getAverageHoles(next));
 				Request[] best = sim5.runBestFit();
-				memR[2] = Double.toString(best.getAverageMemUtil());
-				holeR[2] = Double.toString(best.getAverageHoles());
+				memR[2] = Double.toString(getAverageMemUtil(best));
+				holeR[2] = Double.toString(getAverageHoles(best));
 				Request[] worst = sim5.runWorstFit();
-				memR[3] = Double.toString(worst.getAverageMemUtil());
-				holeR[3] = Double.toString(worst.getAverageHoles());
+				memR[3] = Double.toString(getAverageMemUtil(worst));
+				holeR[3] = Double.toString(getAverageHoles(worst));
 				//Add rows to csv files
 				memRows.add(memR);
 				holeRows.add(holeR);
