325900e8746e10c9cc2a8fdb4c6c1bdb6137e602
Auraiporn <kengauraiporn@gmail.com>
2021-05-07 01:57:43 -0700

    Update initialize method

--- src/Simulation.java
+++ src/Simulation.java
@@ -24,10 +24,40 @@ public class Simulation {
         numHoles = new ArrayList<Integer>();  
     }
     
-    /**
-     * @param n size of physical memory */
-    public void initializeSimulation (int n) {
+    /** Initialize memory to contain a set of blocks of normally distributed sizes
+     *  (using d and v) and placed randomly throughout the memory
+     * 	@param n size of physical memory 
+     */
+    public void initializePhysicalMemory (int n) {
     	physicalMemory = new int[n];
+    	physicalMemory[0] =-n; // 1 block has -n holes
+    	for(int i=0; i<n; i++) {
+    		physicalMemory[i] = getBlockSize(n);  
+    		System.out.print(physicalMemory[i]+ " ");
+    	}  
+    }
+    
+    /** 
+     *  Number of blocks to be deallocated 50% of n
+     * */
+    public int[] randomDeallocation (int n) {
+    	Random random = new Random();
+    	int numberOfBlocksToBeDeallocated = n/2;
+    	Set<Integer> set = new HashSet<Integer>();
+       	do {
+    		set.add(random.nextInt(n-1)); // check for non-duplicates random index
+    	}while (set.size()!= numberOfBlocksToBeDeallocated);
+    	for(Integer i: set) {
+    		int deallocated = physicalMemory[i]*-1;
+    		physicalMemory[i] = deallocated;
+      	}
+      	return physicalMemory;
+     }
+    
+    public void printPhysicalMemory (int[] arr) {
+    	for(Integer i: arr) {
+    		System.out.print(i+ " ");  		
+    	}
     }
     
     /**
@@ -39,8 +69,7 @@ public class Simulation {
     	int max = n-1;
          do {
             s = (int) (random.nextGaussian()*v+d);
-        } while(s>max && s<=0); // discard negative/zero values and n-1 values
-         System.out.println(s);
+        } while(s>max || s<=0); // discard negative/zero values and n-1 values
          return s;
     }
     
@@ -48,11 +77,12 @@ public class Simulation {
      * The method generates the size of each hole which starts with a negative integer, -k, where the absolute value k
      * indicates the length of the hole. 
     */
-    public int generateHoleSize (){
+    public int generateHoleSize (int n){
     	Random random = new Random();
+    	int max = n-1;
         do {
             k = (int) (random.nextGaussian()*v+d);
-        } while ( k>=0 );
+        } while ( k>=0 || k>max);
         System.out.println(k);
         return k;
     }
@@ -63,6 +93,8 @@ public class Simulation {
     public int memoryUtilization(int n){
         return numberOfSpaceOcupied/n;        
     }
+    
+    
    
     
    
